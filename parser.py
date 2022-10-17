aa914093ccda156d19639c1d1a699f58eb466c98
Frantisek Kolecek <xkolec08@stud.fit.vutbr.cz>
2022-10-17 20:08:12 +0200

    PARSER: added support for cancelled type of file, added parsing of at location info

--- parser.py
+++ parser.py
@@ -4,6 +4,9 @@
 import xml.etree.ElementTree as ET, os
 from datetime import datetime, timedelta
 
+# Folder with all xml files
+FOLDER_PATH = "testinputs"
+
 # This function converts start date and bitmapdays
 # To a list of valid dates
 def cal_to_listofdays(calendar):
@@ -14,23 +17,22 @@ def cal_to_listofdays(calendar):
     start = datetime.strptime(startdate, "%Y-%m-%d")
     for i in range(len(binstr)):
         if binstr[i] == '1':
-            ret.append(str(start + timedelta(days=i))[2:10:])
+            ret.append(str(start + timedelta(days=i))[:10:])
     return ret
-
-# Folder with all xml files
-FOLDER_PATH = "testinputs"
 
 # Recursively gathers all data from a node
 # and converts it to a dict
-def node_to_dict(node):
+def node_to_dict(node, ignore=[]):
     ND = {}
     if(len(node) == 0):
         return node.text
     for child in node:
+        if child.tag in ignore:
+            continue
         if child.tag not in ND:
             ND[child.tag] = node_to_dict(child)
         else:
-            if ND[child.tag] is list:
+            if type(ND[child.tag]) is list:
                 ND[child.tag].append(node_to_dict(child))
             else:
                 ND[child.tag] = [ND[child.tag]] + [node_to_dict(child)]
@@ -57,13 +59,44 @@ def parse_file(file):
 
     # Dict with parsed info init
     DT = {}
-    DT["locations"] = []
-    DT["timings"] = []
-    DT["network"] = []
-    DT["creation"] = root.find(".//CZPTTCreation").text
-    DT["ids"] = node_to_dict(root.find(".//Identifiers"))
+
+    # Name of the data origin file
+    DT["filename"] = os.path.basename(file)
+
+    # Type of file - normal or cancel
+    DT["type"] = "cancel" if "cancel" in DT["filename"] else "normal"
+
+    # All identifiers
+    DT["ids"] = [node_to_dict(x) for x in root.findall(".//PlannedTransportIdentifiers")]
+
+    # Raw info about valid days
     DT["calendar"] = node_to_dict(root.find(".//PlannedCalendar"))
+
+    # Same as "calendar" but its parsed to a list of valid days format YYYY-MM-DD
     DT["list_calendar"] = cal_to_listofdays(DT['calendar'])
+    
+    # In cancelation file no more data is found
+    if DT["type"] == "cancel":
+        # Time of cancelation
+        DT["canceled"] = root.find(".//CZPTTCancelation").text
+        return DT
+
+    # List of train stop info (0ty prvek je vychozi stanice, posledni je cilova stanice)
+    DT["locations"] = []
+
+    # List of timings at each locacion (0ty prvek je vychozi stanice, posledni je cilova stanice)
+    # Vychozi a cilova stanice maji jen jeden cas - vychozi odjezd, cilova prijezd
+    # Stanice mezi nimi maji casy oba
+    DT["timings"] = []
+
+    # Network info (not used in this project)
+    DT["network"] = []
+
+    # Time of creation of the file
+    DT["creation"] = root.find(".//CZPTTCreation").text
+
+    # Additional info at each location (0ty prvek je vychozi stanice, posledni je cilova stanice)
+    DT["at_loc_info"] = []
 
     # Parsing
     for location in root.findall(".//Location"):
@@ -72,24 +105,33 @@ def parse_file(file):
         DT["timings"].append(node_to_dict(timing))
     for network in root.findall("./NetworkSpecificParameter"):
         DT["network"].append(node_to_dict(network))
+    for info in root.findall(".//CZPTTLocation"):
+        DT["at_loc_info"].append(node_to_dict(info, ["Location", "TimingAtLocation"]))
     return DT
 
-# Tohle je zatm jen demo, ale parsuje to vsechny xmlka ve slozce
-# urcene glob. promennou FOLDER_PATH
-# pak to za kazdy soubor vytiskne info o vychozi a cilove stanici
-# a calendar, kde je ulozene kdy jsou tyto casy platne
+# Tohle je demo ktere ukazuje jak vypadaji zparsovana data ve vnitrni
+# reprezentaci pythonu.
+# ocekavaji se vstupni .xml soubory ve slozce urcene glob. promennou FOLDER_PATH
 def main():
     filecount = 0
     print("*************************************************")
-    for parsed in parsed_data_generator():
+    for parsed in parsed_data_generator(): # Each loop iteration means one file parsed.
         filecount += 1
-        print(f'ids: {parsed["ids"]}')
-        print(f'Vychozi stanice: {parsed["locations"][0]}\n     {parsed["timings"][0]}\n')
-        print(f'Cilova stanice:  {parsed["locations"][-1]}\n    {parsed["timings"][-1]}\n')
-        print(f'Kalenar: {parsed["calendar"]}\n')
-        print(f'Kalendar v listu: {parsed["list_calendar"]}')
-        print(f'Creation: {parsed["creation"]}')
-        print(f'Network: {parsed["network"]}')
+        if parsed["type"] == "normal":
+            print(f'filename: {parsed["filename"]}')
+            print(f'ids: {parsed["ids"]}')
+            print(f'Vychozi stanice: {parsed["locations"][0]}\n  {parsed["timings"][0]}\n  {parsed["at_loc_info"][0]}\n')
+            print(f'Cilova stanice:  {parsed["locations"][-1]}\n  {parsed["timings"][-1]}\n  {parsed["at_loc_info"][-1]}\n')
+            print(f'Kalenar: {parsed["calendar"]}\n')
+            print(f'Kalendar v listu: {parsed["list_calendar"]}')
+            print(f'Creation: {parsed["creation"]}')
+            print(f'Network: {parsed["network"]}')
+        else: # Canceled type
+            print(f'filename: {parsed["filename"]}')
+            print(f'ids: {parsed["ids"]}')
+            print(f'Kalenar: {parsed["calendar"]}\n')
+            print(f'Kalendar v listu: {parsed["list_calendar"]}')
+            print(f'Canceled: {parsed["canceled"]}')
         print(f"************************************************* {filecount} ***************")
     print(f"Parsed {filecount} files.")
 
